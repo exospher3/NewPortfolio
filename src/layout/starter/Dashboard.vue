95c55c65ca785cf9a016c7e01ae891429ccb9e23
Yassin <exo@Yassins-MacBook-Pro.local>
2020-08-21 14:05:00 -0400

    built for production

--- src/layout/starter/Dashboard.vue
+++ src/layout/starter/Dashboard.vue
@@ -2,11 +2,14 @@
   <div class="card bg-dark text-white">
     <img class="card-img" src="/img/sheridan-trafalgar.jpg" alt="Card image" />
     <div class="card-img-overlay d-flex flex-column bd-highlight">
-      <h5 class="card-title h1 p-2 bd-highlight">Welcome!</h5>
+      <h5 class="card-title h p-2 bd-highlight">Welcome!</h5>
 
-      <p
-        class="shadow-lg card-text h4 bd-highlight mt-auto"
-      >To Yassin Altaha's Portfolio. I am Computer System Analyst graduate from Sheridan College awarded 2nd place in the capstone program for the 2019 year, highly passtionate and motivated toward software development and web deisgn.</p>
+      <p class="shadow-lg card-text h4 bd-highlight mt-auto">
+        To Yassin Altaha's Portfolio. I am Computer System Analyst graduate from
+        Sheridan College awarded 2nd place in the capstone program for the 2019
+        year, highly passtionate and motivated toward software development and
+        web deisgn.
+      </p>
     </div>
   </div>
 </template>
@@ -15,7 +18,7 @@ export default {
   name: "starter-page",
   data() {
     return {};
-  },
+  }
 };
 </script>
 <style></style>
